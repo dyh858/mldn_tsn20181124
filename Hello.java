96349df7176c552b2dfa64c44ef774dd26ebf1f4
dyh858 <dyh858@163.com>
2018-12-01 09:18:24 +0800

    add static mothod

--- Hello.java
+++ Hello.java
@@ -3,4 +3,10 @@ public class Hello {
 		System.out.println("Happy New Year!");
 		System.out.println("Happy Myself!");
 	}
+	public static void print(){
+		System.out.println("*********************") ;
+		System.out.println("*   Hello World !   *") ;
+		System.out.println("*********************") ;
+	}
+
 }
