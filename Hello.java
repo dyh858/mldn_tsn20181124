351f3dc20538430518076bf60e12fac11f40bfc0
dyh858 <dyh858@163.com>
2018-12-01 09:40:49 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,13 +1,6 @@
 public class Hello {
-	public static final COMPANY_NAME="yooth" ; 
 	public static void main(String[] args) {
-		System.out.println("Happy New Year!");
-		System.out.println("Happy Myself!");
+		System.out.println("*********************");
+		System.out.println("#####################");
 	}
-	public static void print(){
-		System.out.println("*********************") ;
-		System.out.println("*   Hello World !   *") ;
-		System.out.println("*********************") ;
-	}
-
 }
