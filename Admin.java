36958933c65b44edcd8dabb06a798d012541f8d6
dyh <dyh858@163.com>
2018-12-06 22:23:08 +0800

    update Admin.java file

--- Admin.java
+++ Admin.java
@@ -1,13 +1,6 @@
 import java.io.* ;
 import java.util.* ;
-public class Emp{
-	private String empno ;
-	private String ename ;
-	private Date hiredate ;
-	private Double sal ;
-	private Double comm ;
-	private Emp mgr ;
-	public String toString(){
-		return "一位雇员。" ;
-	}
+public class Admin{
+	private String aid ;
+	private String password ;
 }
