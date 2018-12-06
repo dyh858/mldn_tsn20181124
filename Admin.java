305917322eba07afc566d5168e537d1ae9626707
dyh <dyh858@163.com>
2018-12-06 22:16:20 +0800

    Add Admin.java File.

---
+++ Admin.java
@@ -0,0 +1,13 @@
+import java.io.* ;
+import java.util.* ;
+public class Emp{
+	private String empno ;
+	private String ename ;
+	private Date hiredate ;
+	private Double sal ;
+	private Double comm ;
+	private Emp mgr ;
+	public String toString(){
+		return "一位雇员。" ;
+	}
+}
